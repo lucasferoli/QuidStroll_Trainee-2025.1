042abff51321b64562494167dc722094d0a59fca
Délio <amarodelio23@gmail.com>
2025-04-29 19:15:11 -0300

    Adiciona parte js

---
+++ public/js/sidebar.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById('sidebar');
+    const menuBotao = document.getElementById('menuMobile');
+    
+    menuBotao.addEventListener('click', () => {
+        sidebar.classList.toggle('active');
+    });
+    
+    document.getElementById('logout').addEventListener('click', (e) => {
+        e.preventDefault();
+        if(confirm('Tem certeza que deseja sair?')) {
+            alert('Você foi desconectado!');
+        }
+    });
+
+});
