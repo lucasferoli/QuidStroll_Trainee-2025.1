15b8eeddeb160b72cbdb1827b03266cdb7bcad46
João Gabriel Duarte G. Almeida <duarte.joao@estudante.ufjf.br>
2025-05-04 20:43:07 -0300

    Adiciona modal do botão "criar usuário".

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+function abrirmodal(criar) {
+    document.getElementById(criar).style.display = "flex";
+}
+
+function fecharmodal(criar) {
+    document.getElementById(criar).style.display = "none";
+}
