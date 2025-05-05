f40709b384482217cf5dbca539a0c01ad1b73f27
Yan Dias <yandiasferreira@gmail.com>
2025-05-05 04:50:07 -0300

    adiciona blur

--- public/js/listaDeUsuarios.js
+++ public/js/listaDeUsuarios.js
@@ -1,6 +1,10 @@
+const tela = document.querySelector('.tela');
+
 function abrirModal(idModal){
     document.getElementById(idModal).style.display="flex";
+    tela.style.display="block";
 }
 function fecharModal(idModal){
     document.getElementById(idModal).style.display="none";
+    tela.style.display="none";
 }
