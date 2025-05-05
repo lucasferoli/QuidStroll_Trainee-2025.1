e1ba814461d2b3252e8010032540aa25693d9987
Yan Dias <yandiasferreira@gmail.com>
2025-05-05 03:23:27 -0300

    transfere logica de abertura/fechamento de arquivo js

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-function abrirmodal(criar) {
-    document.getElementById(criar).style.display = "flex";
-}
-
-function fecharmodal(criar) {
-    document.getElementById(criar).style.display = "none";
-}
