69f47b8fdb4c20696e3868a5e77bbb70fe1b8b04
Délio <amarodelio23@gmail.com>
2025-05-07 19:41:17 -0300

    Adiciona funçao abrir modal de visualizar e adicionar

--- public/js/indexTabelaDePosts.js
+++ public/js/indexTabelaDePosts.js
@@ -19,3 +19,25 @@ function fecharModalExcluir(idJanelaModalExcluir, idFundoModalExcluir){
     document.getElementById(idJanelaModalExcluir).style.display = "none";
     document.getElementById(idFundoModal).style.display = "none";
 }
+
+//Abrir modal Adicionar
+function abrirModal(idJanelaModalAdicionar, idFundoModalAdicionar){
+    document.getElementById(idJanelaModalAdicionar).style.display = "flex";
+    document.getElementById(idFundoModalAdicionar).style.display = "block";
+}
+
+function fecharModal(idJanelaModalAdicionar, idFundoModalAdicionar){
+    document.getElementById(idJanelaModalAdicionar).style.display = "none";
+    document.getElementById(idFundoModalAdicionar).style.display = "none";
+}
+
+//Abrir modal Visualizar
+function abrirModal(idJanelaModalVisualizar, idFundoModalVizualizar){
+    document.getElementById(idJanelaModalVisualizar).style.display = "flex";
+    document.getElementById(idFundoModalVizualizar).style.display = "block";
+}
+
+function fecharModal(idJanelaModalVisualizar, idFundoModalVizualizar){
+    document.getElementById(idJanelaModalVisualizar).style.display = "none";
+    document.getElementById(idFundoModalVizualizar).style.display = "none";
+}
