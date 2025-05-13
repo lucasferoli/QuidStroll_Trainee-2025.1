18d0ce19b1cd2f30ccca00658d689b25370c9c15
lucasferoli <lucasferoliveira.goval@gmail.com>
2025-05-13 14:03:32 -0300

    Termina de configurar o php para a database funcionar

--- app/Controllers/LandingController.php
+++ app/Controllers/LandingController.php
@@ -4,12 +4,16 @@ namespace App\Controllers;
 
 use App\Core\App;
 use Exception;
-
+use var_dump;
+use App\core\database\Querybuilder;
 class LandingController
 {
 
     public function index()
     {
+        $queryBuilder = new Querybuilder();
+        $user = $queryBuilder->selectAll('usuarios');
+        var_dump($user);
         return view('site/landingPage');
     }
 }
