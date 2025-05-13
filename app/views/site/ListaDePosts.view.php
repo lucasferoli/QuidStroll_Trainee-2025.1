48ce02e260329379ee4b7a3d73b62601e6be0d6a
João Gabriel Duarte G. Almeida <duarte.joao@estudante.ufjf.br>
2025-05-13 20:48:19 -0300

    Altera paginação para novo design padronizado

---
+++ app/views/site/ListaDePosts.view.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quid Stroll</title>
+    <link rel="stylesheet" href="/public/css/listaDePosts.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Rubik:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+</head>
+<body>
+    <div class="segBarraDePesquisa">
+        <div class="barraDePesquisa">
+            <input type="text" placeholder="Barra de pesquisa...">
+            <i class="fas fa-search"></i>
+        </div>
+    </div>
+
+    <div class="grid">
+        <div class="card1">
+            <div class="imagem"></div>
+            <div class="conteudo">
+                <h3>Lorem Ipsum Dolor</h3>
+                <p>Descrição: Lorem Ipsum Dolor</p>
+            </div>
+            <div class="autor">Autor: ...</div>
+        </div>
+        <div class="card">
+            <div class="imagem"></div>
+            <div class="conteudo">
+                <h3>Lorem Ipsum Dolor</h3>
+                <p>Descrição: Lorem Ipsum Dolor</p>
+            </div>
+            <div class="autor">Autor: ...</div>
+        </div>
+        <div class="card">
+            <div class="imagem"></div>
+            <div class="conteudo">
+                <h3>Lorem Ipsum Dolor</h3>
+                <p>Descrição: Lorem Ipsum Dolor</p>
+            </div>
+            <div class="autor">Autor: ...</div>
+        </div>
+        <div class="card">
+            <div class="imagem"></div>
+            <div class="conteudo">
+                <h3>Lorem Ipsum Dolor</h3>
+                <p>Descrição: Lorem Ipsum Dolor</p>
+            </div>
+            <div class="autor">Autor: ...</div>
+        </div>
+        <div class="card">
+            <div class="imagem"></div>
+            <div class="conteudo">
+                <h3>Lorem Ipsum Dolor</h3>
+                <p>Descrição: Lorem Ipsum Dolor</p>
+            </div>
+            <div class="autor">Autor: ...</div>
+        </div>
+        <div class="card">
+            <div class="imagem"></div>
+            <div class="conteudo">
+                <h3>Lorem Ipsum Dolor</h3>
+                <p>Descrição: Lorem Ipsum Dolor</p>
+            </div>
+            <div class="autor">Autor: ...</div>
+        </div>
+    </div>
+    <nav class="paginacao-listaDePosts">
+            <button>&lt</button>
+            <button id="paginaEmUso-listaDeUsuarios">1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>&gt</button>
+        </nav>
+</body>
+</html>
