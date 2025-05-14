a1a6295d59258053649a5d57f6de1dfe3db165f6
Yan Dias <yandiasferreira@gmail.com>
2025-05-14 16:52:15 -0300

    altera hover do cabeçalho e espaçamento da tabela de conteudo

--- public/js/postIndividual.js
+++
@@ -1,8 +0,0 @@
-const link=document.getElementById("postIndividualSeta");
-
-link.addEventListener('mouseover', () => {
-    link.style.backgroundColor = 'rgba(133, 193, 233)'
-});
-link.addEventListener('mouseout', () => {
-    link.style.backgroundColor = '';
-});
