f2b37dd21e884aa3f715eed40489ad1d54cb869b
João Gabriel Duarte G. Almeida <duarte.joao@estudante.ufjf.br>
2025-05-20 17:14:52 -0300

    Adiciona rota dos Usuarios

--- app/routes.php
+++ app/routes.php
@@ -8,3 +8,4 @@ $router->get('', 'LandingController@index');
 
 $router->get('login', 'LoginController@index');
 $router->get('dashboard', 'DashboardController@index');
+$router->get('crudUsuarios', 'UsuariosController@index');
