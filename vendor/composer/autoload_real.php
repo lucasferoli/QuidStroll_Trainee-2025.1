8cc2866ce109d724701feaad09ede8953a183226
Yan Dias <yandiasferreira@gmail.com>
2025-05-20 19:59:04 -0300

    aparece automaticamente pós composer autoload

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit90bbf95869b4e88bf0dc5022e708a458
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit90bbf95869b4e88bf0dc5022e708a458', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit90bbf95869b4e88bf0dc5022e708a458', 'loadClassLoader'));
