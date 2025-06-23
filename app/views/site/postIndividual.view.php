53356f0f9d6e55ac624d2f94e3a273e738bfbf7e
Deyvison Fernandes <deyvisonotaviano@gmail.com>
2025-06-22 22:58:44 -0300

    Realiza mais alguns poucos toques finais

--- app/views/site/postIndividual.view.php
+++ app/views/site/postIndividual.view.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Post Individual</title>
     <link rel="stylesheet" href="/public/css/postIndividual.css">
     <script src="/public/js/postIndividual.js" defer></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
